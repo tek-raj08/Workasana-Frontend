45e77b93ebb9c921b737d14caac45c1f636d5c38
tek-raj08 <ranatekraj06@gmail.com>
2025-06-06 12:29:55 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://workasana-backend-git-main-tek-rajs-projects.vercel.app"
+// export const BASE_URL = "https://workasana-backend-git-main-tek-rajs-projects.vercel.app"
+export const BASE_URL = "http://localhost:3000"
