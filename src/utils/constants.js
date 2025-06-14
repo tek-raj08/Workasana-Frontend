4eb4ed83283f0cf010048fd38b9daa9ff5364521
Tek Raj <ranatekraj06@gmail.com>
2025-06-14 09:59:30 +0530

    update the backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,4 @@
-export const BASE_URL = "https://workasana-backend-git-main-tek-rajs-projects.vercel.app"
-// <<<<<<< HEAD
+
 // export const BASE_URL = "http://localhost:3000"
-// =======
-// export const BASE_URL = "http://localhost:3000"
-// export const BASR_URL = "https://workasana-backend-48ytzkn7j-tek-rajs-projects.vercel.app"
-// >>>>>>> cd1fbd2282e3a266f570f4900098ed0698d7d629
+export const BASE_URL = "https://workasana-backend-nu.vercel.app"
+
