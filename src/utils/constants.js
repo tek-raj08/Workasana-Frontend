ae8b5a5998adbf8ab928ab9842cd5a760c8c8f0b
Tek Raj <ranatekraj06@gmail.com>
2025-06-14 11:17:05 +0530

    CC

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 
-export const BASE_URL = "http://localhost:3000"
-// export const BASE_URL = "https://workasana-backend-nu.vercel.app"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://workasana-backend-nu.vercel.app"
 
